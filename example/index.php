8cdef6d1a015cd4610b997811430fac7f2bd28a1
ngohr <nanno.gohr@gmail.com>
2014-11-14 12:08:40 +0100

    no comment

--- example/index.php
+++ example/index.php
@@ -3,19 +3,23 @@
 // Include xsltDocument main.php
 require_once('../castor/Castor.php');
 
-// Load with a config file
-$document = new xsltDocument('config/config.xml', 'home');
-$document->loadPage();
+try {
+	// Load with a config file
+	$document = new xsltDocument('config/config.xml', 'home');
+	$document->loadPage();
 
-// Alternative (this time only when an array will returned)
-// $document = new phtmlDocument('config/config.xml', 'home');
-// $document->loadPage();
+	// Alternative (phtmlDocument does not support DomDocument Objects)
+	// $document = new phtmlDocument('config/config.xml', 'home');
+	// $document->loadPage();
 
-// Alternative
-// $page = new Page('home', 'index');
-// $page->addFile('controller/indexController.php');
-// $page->setElement('myelement', array('element' => 'Hello World'));
-// $page->addAction('index', 'controller/indexController.php', 'indexAction');
-// $page->loadPage('home', 'index');
+	// Alternative
+	// $page = new Page('home', 'index');
+	// $page->addFile('controller/indexController.php');
+	// $page->setElement('myelement', array('element' => 'Hello World'));
+	// $page->addAction('index', 'controller/indexController.php', 'indexAction');
+	// $page->loadPage('home', 'index');
 
-// thx and good luck
+	// thx and good luck
+} catch(Exception $exeption) {
+	die($exeption->getMessage());
+}
