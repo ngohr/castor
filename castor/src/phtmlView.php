146027579685f6f86f82a1bd4965974c92c52c1d
ngohr <nanno.gohr@gmail.com>
2014-11-17 08:47:41 +0100

    absence

--- castor/src/phtmlView.php
+++ castor/src/phtmlView.php
@@ -3,15 +3,29 @@
 class View {
 	static $elements = array();
 
-	static function Add($value) {
-		self::$elements[$index] = $value;
+	static function Add($name, $element) {
+		if(!array_key_exists($name, self::$elements)) {
+			self::$elements[$name] = $element;
+
+			return true;
+		}
+
+		return false;
 	}
-	
-	static function Get($index) {
-		if(array_key_exists(index, self::$elements))
-			self::$elements[$index];
-		else
-			return false;
+
+	static function Get($name = false, $index = false) {
+		if(!$name) {
+			return self::$elements;
+		}
+
+		if(array_key_exists($name, self::$elements)) {
+			if(!$index) {
+				return self::$elements[$name];
+			} else {
+				return self::$elements[$name][$index];
+			}
+		}
+
+		return false;
 	}
-	
 }
