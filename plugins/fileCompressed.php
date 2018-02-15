c9524185cc7d770a3e8f3097f9b6ac35564a4526
Nanno Gohr <nanno.gohr@gmail.com>
2018-02-15 17:26:14 +0100

    ob
    Fixed busy output buffering when a controller starts an output

--- plugins/fileCompressed.php
+++ plugins/fileCompressed.php
@@ -1,33 +1,5 @@
 <?php
 
-/*
- * Copyright (c) 2016, Nanno Gohr
- *
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of  nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific
- * prior written permission.
-
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
 define('FILE_TIMEOUT', 0);
 
 class HttpCache extends Application {
@@ -301,8 +273,8 @@ function CompressJavascript(&$objPage, $action) {
 }
 
 function get_include_contents($filename) {
-	if (is_file($filename)) {
-		ob_start();
+	if(is_file($filename)) {
+		ob_start(NULL, 0, PHP_OUTPUT_HANDLER_FLUSHABLE ^ PHP_OUTPUT_HANDLER_REMOVABLE);
 		include $filename;
 		return ob_get_clean();
 	}
@@ -310,6 +282,7 @@ function get_include_contents($filename) {
 }
 
 function CompressPhtml(&$objPage, $action) {
+	header("Content-type: text/html; charset=UTF-8", true);
 	$returnvalue = $objPage->call($action);
 	if($returnvalue && is_array($returnvalue)) {
 		phtmlDocument::addArray($returnvalue);
@@ -319,37 +292,39 @@ function CompressPhtml(&$objPage, $action) {
 		}
 	}
 
+	ob_implicit_flush(false);
 	$stylesheet = $objPage->getStylesheet($action);
 	$content = get_include_contents($stylesheet);
+	if($content) {
+		$content = str_replace("\t", "", $content);
+		$content = str_replace("\r", "", $content);
+		$content = preg_replace("/\>\n\</m", "><", $content);
+		$content = str_replace("\s*\n*", "\s", $content);
+		$content = preg_replace("/<\!--.+?-->/sm", "", $content);
+		if(preg_match('/(\<script.+?type\=\"text\/javascript\".*?\>.+?)\/\*.+?\*\/(.+?\<\/script\>)/ism', $content, $match))
+			$content = preg_replace('/(\<script.+?type\=\"text\/javascript\".*?\>.+?)\/\*.+?\*\/(.+?\<\/script\>)/ism', "$1$2", $content);
+		while(preg_match('/(<style.+?>.*?)\{\n(.*?<\/style>)/ism', $content))
+			$content = preg_replace('/(<style.+?>.*?)\{\n(.*?<\/style>)/ism', '$1{$2', $content);
+		while(preg_match('/(<style.+?>.*?)\;\n(.*?<\/style>)/ism', $content))
+			$content = preg_replace('/(<style.+?>.*?)\;\n(.*?<\/style>)/ism', "$1;$2", $content);
+		while(preg_match('/(<style.+?>.*?)\}\n\}(.*?<\/style>)/ism', $content))
+			$content = preg_replace('/(<style.+?>.*?)\}\n\}(.*?<\/style>)/ism', "$1}}$2", $content);
+		while(preg_match('/\n{2,}/mis', $content))
+			$content = str_replace("\n\n", "\n", $content);
 
-	$content = str_replace("\t", "", $content);
-	$content = str_replace("\r", "", $content);
-	$content = preg_replace("/\>\n\</m", "><", $content);
-	$content = str_replace("\s*\n*", "\s", $content);
-	$content = preg_replace("/<\!--.+?-->/sm", "", $content);
-	if(preg_match('/(\<script.+?type\=\"text\/javascript\".*?\>.+?)\/\*.+?\*\/(.+?\<\/script\>)/ism', $content, $match))
-		$content = preg_replace('/(\<script.+?type\=\"text\/javascript\".*?\>.+?)\/\*.+?\*\/(.+?\<\/script\>)/ism', "$1$2", $content);
-	while(preg_match('/(<style.+?>.*?)\{\n(.*?<\/style>)/ism', $content))
-		$content = preg_replace('/(<style.+?>.*?)\{\n(.*?<\/style>)/ism', '$1{$2', $content);
-	while(preg_match('/(<style.+?>.*?)\;\n(.*?<\/style>)/ism', $content))
-		$content = preg_replace('/(<style.+?>.*?)\;\n(.*?<\/style>)/ism', "$1;$2", $content);
-	while(preg_match('/(<style.+?>.*?)\}\n\}(.*?<\/style>)/ism', $content))
-		$content = preg_replace('/(<style.+?>.*?)\}\n\}(.*?<\/style>)/ism', "$1}}$2", $content);
-	while(preg_match('/\n{2,}/mis', $content))
-		$content = str_replace("\n\n", "\n", $content);
-
-		/*
-	$content = str_replace(" 0px", "0", $content);
-	$content = str_replace(" 0%", "0", $content);
-	$content = str_replace(" 0em", "0", $content);
-	$content = str_replace(":0px", "0", $content);
-	$content = str_replace(":0%", "0", $content);
-	$content = str_replace(":0em", "0", $content);
-	*/
-	header("Content-type: text/html; charset=UTF-8", true);
-	echo $content;
+			/*
+		$content = str_replace(" 0px", "0", $content);
+		$content = str_replace(" 0%", "0", $content);
+		$content = str_replace(" 0em", "0", $content);
+		$content = str_replace(":0px", "0", $content);
+		$content = str_replace(":0%", "0", $content);
+		$content = str_replace(":0em", "0", $content);
+		*/
+		echo $content;
+	}
 
 	ob_end_flush();
+	// flush();
 }
 
 hooks::addHook('returnType', 'CompressCss', 'CompressCss');
